a769624ea6262e9f1c89296e9fa86f534f87d2d6
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-04 23:03:04 -0700

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints _putcha
+ *
+ * created : Sanctus-Peter
+ * cc: 5 july, 2022
+ *
+ * Return: (0) if successful
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
