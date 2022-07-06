0d7ad63bea233f2242d844e8b9649247c1e35865
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-06 02:06:55 -0700

    largest of 3 number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -21,7 +21,7 @@ int largest_number(int var1, int var2, int var3)
 		largest = var1;
 	else if (var2 > var1 && var2 > var3)
 		largest = var2;
-	else
+	else if (var3 > var1 && var3 > var1)
 		largest = var3;
 
 	return (largest);
