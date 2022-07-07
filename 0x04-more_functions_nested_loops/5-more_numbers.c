31e93cf4efa58cf1a65780280d80602f8bb88ea4
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-07 05:25:31 -0700

    numbers up to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers up to 14
+ *
+ * Created by: Sanctus-Peter
+ * cc: 7th july, 2022
+ */
+
+void more_numbers(void)
+{
+	int ii = 0, jj = 0;
+
+	while (jj < 10)
+	{
+		do {
+			if (ii < 10)
+				_putchar(ii + '0');
+			else
+			{
+				_putchar((ii / 10) + '0');
+				_putchar((ii % 10) + '0');
+			}
+
+			ii++;
+		} while (ii < 15);
+
+		_putchar('\n');
+	}
+}
