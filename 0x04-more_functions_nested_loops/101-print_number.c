ae0d1adf92114393c5bb1b20121e24eaba1e4cfe
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-07 14:53:16 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,30 +10,16 @@
 
 void print_number(int n)
 {
-
-	int d = 1, i = 0, ii = 0;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	while (n / d != 0)
-	{
-		d *= 10;
-		i++;
-	}
-	d = d / 10;
+	if (n == 0)
+		_putchar('0');
 
-	while (ii < i)
-	{
-		_putchar('0' + n / d);
-		n = n - (n / d) * d;
-		d = d / 10;
-		ii++;
-	}
-
-	if (i == 0)
-		_putchar('0' + n);
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
