9443ac3cc15674a5f10431c724b5b19c1295edbe
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-07 15:04:57 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_number - print an integer
+ * print - print an integer
  * @n: the integer to be printed
  *
  * Created by: Sanctus-Peter
  * cc: 7th july, 2022
  */
 
-void print_number(int n)
+void print(int n)
 {
 	if (n < 0)
 	{
@@ -20,6 +20,15 @@ void print_number(int n)
 		_putchar('0');
 
 	if (n / 10)
-		print_number(n / 10);
+		print(n / 10);
 	_putchar(n % 10 + '0');
+}
+
+/**
+ * print_number - print num
+ * @var: var
+ */
+void print_number(int var)
+{
+	print(var);
 }
