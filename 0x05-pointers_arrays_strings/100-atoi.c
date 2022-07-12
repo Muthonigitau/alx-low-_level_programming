6cd64c68c4fa052cf2e0fe9f7fc1fef8121dbbf1
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-12 03:53:08 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _atoi - coverts a string to integer
+ * @str: pointer to string to be converted
+ *
+ * Return: converted string
+ *
+ * Created by: Sanctus-Peter
+ * cc: 12th july, 2022.
+ */
+
+int _atoi(char *str)
+{
+	return (atoi(str));
+}
