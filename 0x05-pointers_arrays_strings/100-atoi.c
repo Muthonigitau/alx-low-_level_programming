ea0d33459af7a22cc824ea7cc0a5fcb8235896dc
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-12 04:24:10 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - coverts a string to integer
@@ -13,5 +14,29 @@
 
 int _atoi(char *str)
 {
-	return (atoi(str));
+	int sign = 1, base = 0, i = 0;
+
+	while (str[i] == ' ')
+	{
+		i++;
+	}
+
+	if (str[i] == '-' || str[i] == '+')
+	{
+		sign = 1 - 2 * (str[i++] == '-');
+	}
+
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (base > INT_MAX / 10	|| (base == INT_MAX / 10
+			&& str[i] - '0' > 7))
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+		base = 10 * base + (str[i++] - '0');
+	}
+	return (base * sign);
 }
