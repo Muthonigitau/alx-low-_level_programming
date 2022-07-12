40824dcfd55a3d40c046ed0586de620c1a407a0e
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-12 03:06:31 -0700

    other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,14 @@ void puts_half(char *s)
 {
 	int len = strlen(s), i;
 
-	for (i = len / 2; i < len; i++)
-		_putchar(s[i]);
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+			_putchar(s[i]);
+	}
+	else
+		for (i = (len / 2) + 1; i < len; i++)
+			_putchar(s[i]);
+
 	_putchar('\n');
 }
