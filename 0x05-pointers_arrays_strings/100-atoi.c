6d5aa94d233b2192ed582adb78bae3a8be3dcf34
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-12 05:48:56 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 #include <limits.h>
+#include <string.h>
 
 /**
  * _atoi - coverts a string to integer
@@ -16,11 +17,17 @@ int _atoi(char *str)
 {
 	int sign = 1, base = 0, i = 0;
 
-	for (i = 0; (str[i] < '0' || str[i] > '9'); i++)
+	if (strlen(str) < 1)
+		return (0);
+
+	for (i = 0; str[i] != '\0' && (str[i] < '0' || str[i] > '9'); i++)
 	{
 		if (str[i] == '-' || str[i] == '+')
 			sign *= 1 - 2 * (str[i] == '-');
 	}
+
+	if (str[i - 1] == '\0')
+		return (0);
 
 	while (str[i] >= '0' && str[i] <= '9')
 	{
