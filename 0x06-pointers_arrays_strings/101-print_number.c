8706c71c63029a48cdf01583c51fb15381be3269
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-15 06:28:15 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,22 +9,13 @@
 void print_number(int n)
 {
 	unsigned int n1;
-	int divisor = 1, i, resp;
-
+	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
 		n1 = -n;
 	}
-
-	n1 = n;
-
-	for (i = 0; n1 / divisor > 9; i++, divisor *= 10);
-
-	for (; divisor >= 1; n1 %= divisor, divisor /= 10)
-	{
-		resp = n1 / divisor;
-		_putchar('0' + resp);
-
-	}
+	if (n1 / 10 != 0)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
 }
