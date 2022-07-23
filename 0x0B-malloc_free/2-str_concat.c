b6c206a6a7cfd4b37eebe4b7c39048ca19b12bae
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-23 09:41:50 -0700

    str-concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include "main.h"
 
+int _strlen(const char *s);
+
 /**
  * str_concat - concatenate two string
  * @s1: pointer to first string
@@ -19,15 +21,14 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0;
 	char *output;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "\0";
+	else if (s2 == NULL)
+		s2 = "\0";
+	i = _strlen(s1);
+	j = _strlen(s2);
 
-	for (; s1[i] != '\0'; i++)
-		;
-	for (; s2[j] != '\0'; j++)
-		;
-
-	output = malloc((i + j) * sizeof(*s1) + 2);
+	output = malloc((i + j) * sizeof(*s1) + 1);
 
 	if (output == 0)
 		return (NULL);
@@ -35,4 +36,17 @@ char *str_concat(char *s1, char *s2)
 	strcat(output, s2);
 
 	return (output);
+}
+/**
+ * _strlen - get  the length of the string s
+ * @s: pointer to the string whose length is required
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		;
+	return (i);
 }
