ab213b8e8185c2e0412530f3c9854503ecf7e066
Sanctus-Peter <sanctuspeter@gmail.com>
2022-07-23 11:30:30 -0700

    2-D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,14 +17,14 @@ int **alloc_grid(int col, int row)
 {
 	int i, j,  **grid;
 
-	grid = malloc(col * sizeof(*grid));
+	grid = malloc(row * sizeof(*grid));
 
 	if (row < 1 || col < 1 || grid == 0)
 		return (NULL);
 
 	for (i = 0; i < row; i++)
 	{
-		grid[i] = malloc(row * sizeof(**grid));
+		grid[i] = malloc(col * sizeof(**grid));
 		if (grid[i] == 0)
 		{
 			while (i--)
