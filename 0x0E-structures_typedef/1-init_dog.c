d5318fdeeea9dc5c04c0423eed295e1ed5b6356c
Sanctus-Peter <sanctuspeter@gmail.com>
2022-08-01 11:03:34 -0700

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: pointer to dog name vaariable
+ * @name: pointer to the name variable
+ * @age: age of the dog
+ * @owner: pointer to the name variable of the onwer of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		printf("Error: no variable entered, please supply a variable for d");
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
