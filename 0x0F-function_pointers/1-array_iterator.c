2fda84e8b0390496f0d0a80c99503aada72cd99f
Sanctus-Peter <sanctuspeter@gmail.com>
2022-08-03 06:50:58 -0700

    function call back on array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -
+ * array_iterator - function to act on an array of element
  * @array: an array of element of type int
  * @size: size of the array
  * @action: callback function pointing to another function
