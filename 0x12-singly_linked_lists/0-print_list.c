68104206b98ff01daddd7a5fb2812f495a18f607
Sanctus-Peter <sanctuspeter@gmail.com>
2022-08-11 10:37:31 -0700

    betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,14 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	const list_t *current_node = h;
 
-	while (current_node)
+	while (h)
 	{
-		if (current_node->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		printf("[%d] %s\n", current_node->len, current_node->str);
+		printf("[%d] %s\n", h->len, h->str);
 		count++;
-		current_node = current_node->next;
+		h = h->next;
 	}
 	return (count);
 }
