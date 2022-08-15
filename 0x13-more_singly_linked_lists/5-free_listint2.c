79aca57edb1611182830ddc19451509a06c3c8e0
Sanctus-Peter <sanctuspeter@gmail.com>
2022-08-15 12:17:25 -0700

    task 0 - 10

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,8 +14,8 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		current = *head;
-		*head = (*head)->next;
-		free(current);
+		current = (*head)->next;
+		free(*head);
+		*head = current;
 	}
 }
