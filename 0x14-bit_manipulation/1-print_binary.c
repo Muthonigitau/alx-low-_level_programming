e50c354c30bb9a29d34e739bf5aee7e92bf935a6
Sanctus-Peter <sanctuspeter@gmail.com>
2022-08-18 05:08:04 -0700

    solved without using array

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,13 @@
 #include "main.h"
 
-void print_b_helper(unsigned long int n, unsigned long int i);
 /**
- * print_binary - prints an int in binary
- * @n: number to print
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be printed in binary
  */
 void print_binary(unsigned long int n)
 {
-	if (!n)
-	{
-		_putchar('0');
-		return;
-	}
-	print_b_helper(n, 1);
-}
+	if (n > 1)
+		print_binary(n >> 1);
 
-/**
- * print_b_helper - prints an int in binary
- * @n: number to print
- * @i: number to compare (in powers of 2)
- */
-void print_b_helper(unsigned long int n, unsigned long int i)
-{
-	if (i > n)
-		return;
-	print_b_helper(n, i << 1);
-	_putchar(n & i ? '1' : '0');
+	_putchar((n & 1) + '0');
 }
